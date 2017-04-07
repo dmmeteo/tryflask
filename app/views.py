2678081f14182ff01d1287d64cac15df7c6a29f9
Dmitry Sichkar <dmmeteo@gmail.com>
2017-04-08 02:57:15 +0300

    2. Templates

--- app/views.py
+++ app/views.py
@@ -1,6 +1,22 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return 'Hello, World!'
+	user = {'name': 'Moo'}
+	posts = [
+        { 
+            'author': { 'name': 'John' }, 
+            'body': 'Beautiful day in Portland!' 
+        },
+        { 
+            'author': { 'name': 'Susan' }, 
+            'body': 'The Avengers movie was so cool!' 
+        }
+    ]
+	return render_template('index.html', 
+		title='Home',
+		user=user,
+		posts=posts)
